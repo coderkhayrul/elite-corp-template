d4243f2c209c98bb2f4a14c8b07128bf1778dea2
coderkhayrul <coderkhayrul@gmail.com>
2022-03-27 00:41:00 +0600

    Boostrap html template

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+// Work Slider
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    center:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+// Work Slider End
